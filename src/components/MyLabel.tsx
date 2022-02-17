1a1e7917941c7953f0233e4374029eb5f7877f6c
BauTancredi <bautistatancredi@hotmail.com>
2022-02-17 09:52:14 -0300

    storybook ready

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,38 @@
+import "./mylabel.css";
+
+export interface MyLabelProps {
+  /**
+   * Display value of the label
+   */
+  label: string;
+  /**
+   * Size of the label
+   */
+  size?: "normal" | "h1" | "h2" | "h3";
+  /**
+   * Label capitalized
+   */
+  allCaps?: boolean;
+  /**
+   * Color of the label
+   */
+  color?: "primary" | "secondary" | "tertiary";
+  /**
+   * Custom color of the label
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  label = "No label",
+  size = "normal",
+  allCaps = false,
+  color = "primary",
+  fontColor,
+}: MyLabelProps) => {
+  return (
+    <span className={`label ${size} text-${color}`} style={{ color: fontColor }}>
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  );
+};
